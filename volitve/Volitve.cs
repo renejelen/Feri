f5b17cd8583364d707c3460f7fde3ac528e20e9c
renejelen <150295663+renejelen@users.noreply.github.com>
2024-08-25 13:45:02 +0200

    Add files via upload

---
+++ volitve/Volitve.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volitve
+{
+    public class Volitve
+    {
+        public List<Stranka> strankaList { get; set; }
+        public List<Kandidat> kandidatList { get; set; }
+        public List<Volilec> volilecList { get; set; }
+
+        public Volitve(List<Stranka> strankaList, List<Kandidat> kandidatList, List<Volilec> volilecList)
+        {
+            this.strankaList = strankaList;
+            this.kandidatList = kandidatList;
+            this.volilecList = volilecList;
+        }
+
+        public List<Volilec> volilniUpravicenci()
+        {
+            return volilecList.Where(v => v.starost >= 18).ToList();
+        }
+
+        public void najstarejsiKandidat()
+        {
+            var najstarejsiKandidat = kandidatList.OrderByDescending(k =>  k.starost).FirstOrDefault();
+            if(najstarejsiKandidat != null)
+            {
+                Console.WriteLine($"\nNajstarejsi kandidat: {najstarejsiKandidat.ime}, {najstarejsiKandidat.starost}");
+            }
+        }
+
+        public void kandidatIzbrStranke(Stranka stranka)
+        {
+            var izbranKandidat = kandidatList.Where(k => k.stranka.ime == stranka.ime).ToList();
+
+            Console.WriteLine($"\nKandidati stranke {stranka.ime}:");
+            foreach (Kandidat k in izbranKandidat)
+            {
+                Console.WriteLine($"{k.ime}, {k.stranka.ime}");
+            }
+        }
+
+        public void izvedba()
+        {
+            Random rand = new Random();
+            Console.WriteLine("\nRezultati volitev:");
+            foreach (Volilec v in volilniUpravicenci())
+            {
+                v.glas = kandidatList[rand.Next(kandidatList.Count)];
+                v.glas.steviloGlasov++;
+                Console.WriteLine($"Volilec {v.ime} je glasoval za {v.glas.ime} {v.glas.stranka.ime}");
+            }
+        }
+
+        public void rezultati()
+        {
+            var steviloGlasov = volilecList.Count(v => v.glas != null);
+            Console.WriteLine("\nRezultati volitev:");
+
+            foreach(Kandidat kandidat in kandidatList.OrderByDescending(k => k.steviloGlasov)) 
+            {
+                double procentniDelez = (double)kandidat.steviloGlasov / steviloGlasov*100;
+                Console.WriteLine($"{kandidat.ime} ({kandidat.stranka.ime}): {kandidat.steviloGlasov}");
+            }
+        }
+
+        public void udelezba()
+        {
+            double udelezba = (double)volilecList.Count(v => v.glas != null) / volilecList.Count * 100;
+            Console.WriteLine($"\nUdelezba na volitvah: {udelezba:F2}%");
+        }
+
+        public void delezPosameznegaSpola()
+        {
+            var zmagovalec = kandidatList.OrderByDescending(c => c.steviloGlasov).First();
+            var moskiVolilci = volilecList.Count(v => v.glas.ime == zmagovalec.ime && v.spol == 'M');
+            var zenskiVolilci = volilecList.Count(v => v.glas.ime == zmagovalec.ime && v.spol == 'Z');
+            var volilciZmagovalca = moskiVolilci + zenskiVolilci;
+
+            double malePercentage = (double)moskiVolilci / volilciZmagovalca * 100;
+            double femalePercentage = (double)zenskiVolilci / volilciZmagovalca * 100;
+
+            Console.WriteLine($"\nZmagovalec: {zmagovalec.ime}");
+            Console.WriteLine($"Moski volilci zmagovalca: {malePercentage:F2}%");
+            Console.WriteLine($"Zenski volilci zmagovalca: {femalePercentage:F2}%");
+        }
+    }
+}
